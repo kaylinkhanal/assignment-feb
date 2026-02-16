4c677078cc1efb6717d5866b7a80d2cefc260149
Santosh Parajuli <santosh.08045@gmail.com>
2026-02-16 08:46:17 +0545

    array level 1 questions

--- array/level1.js
+++ array/level1.js
@@ -2,9 +2,13 @@ const arr  = [1,2,4,12,14]
 // Q1. remove the last item of the array, 
 // hint use pop() method
 
+arr.pop();
+console.log(arr);
 
-// Q2. Add new value 55 to the end of the array, hint use push() method
 
-
+// Q2. Add new value 55 to the end of the array, hint use push()
+arr.push(55);
+console.log(arr);
 
 // Q3. Find the length of the array, expected output is 4
+console.log(arr.length);
