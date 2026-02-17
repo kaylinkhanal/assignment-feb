1f670f47e045cf3c70ad5982ef3cba89f7aae24c
Alisa Bogati <alaskabogati@Alishas-MacBook-Air.local>
2026-02-17 19:38:24 +1100

    Js array level 1 and 2

---
+++ array/level2.js
@@ -0,0 +1,17 @@
+const scores = [32,34,50]
+
+// Q1. calculate the total score and assign it to a variable called totalScore
+// hint: scores[0]+ scores[1]+ ...
+const totalScore = scores[0]+ scores[1] + scores[2]
+console.log(totalScore)
+
+//Q2. calcuate the percentage of the total score, expected output is 72.22%, total score is out of 150
+const total= 150
+const percent= (totalScore/total )* 100 
+console.log(percent.toFixed(2))
+
+
+const scoresOptional = [32,34]
+// Q3. merge scores and scoresOptional into a new array called allScores, expected output is [32,34,50,32,34]
+const mergedarr= [...scores, ...scoresOptional]
+console.log(mergedarr)
