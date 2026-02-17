5d013992e46c616681bc1893873a2e3afedb3d4e
DikpalChaudhary <you@example.com>
2026-02-17 15:46:32 +0545

    level4

---
+++ strings/level4.js
@@ -0,0 +1,7 @@
+// QUESTION 1: First Three Elements
+const months = ["January", "February", "March", "April", "May", "June"]
+// Expected output: ["January", "February", "March"]
+const result =months.slice(0,3)
+console.log(result)
+
+
