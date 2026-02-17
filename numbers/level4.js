e3c4a8f13a08b9715d0338924d34735ade72471b
Aalok-sah14 <aaloksah1003@gmail.com>
2026-02-17 16:48:48 +0545

    answer of level 4

--- numbers/level4.js
+++ numbers/level4.js
@@ -1,16 +1,22 @@
 /**Q1: The Tax Calculator Function Create a function called calculateTotal that takes a price and a taxRate. 
   It should return the total price rounded to 2 decimal places. Example: calculateTotal(100, 0.15) should return 115.00.*/
   
-
+  function calculateTotal(amount, percentage)
+  {
+    return amount + (amount * percentage);
+  }
+  const result = calculateTotal(100, 0.15);
+  console.log(result)
+  console.log(result.toFixed(2))
 
 
 
 /*Q2: The "Even" Filter Create an array of numbers: [1, 2, 3, 4, 5, 6]. 
   Use a loop or an array method to create a new array that only contains the even numbers.*/
+const numbers = [1, 2, 3, 4, 5, 6]
+const evenNumbers = numbers.filter(number => number % 2 === 0)
+console.log(evenNumbers)
 
 
 
 
-
-/*Q3: Object Mapping Create an object representing a car with properties for brand, model, and year.
-  Write a script that checks if the car is "antique" (older than 25 years from the current year 2026). */
--- strings/level6.js
+++ strings/level6.js
@@ -2,10 +2,16 @@ const arr = ["apple", "banana", "grape", "orange", "kiwi"]
 
 
 // Q1 . expected output is "apple, banana, grape, orange, kiwi"
+const output = arr.join(", ")
+console.log(output)
 
 // Q2. expected output is "APPLE"
-
+const capital = arr.map(arr => arr.toUpperCase());
+console.log(capital);
 
 
 const scores  = "32,42,53"
+
 // Q3. Expected output is [32,42,53]
+const output1 = scores.split(',').map(Number);
+console.log(output1)
