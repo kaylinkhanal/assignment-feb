ed40d08954c0fbab3c319716a6821abd316c1837
Aalok-sah14 <aaloksah1003@gmail.com>
2026-02-17 19:50:08 +0545

    answer of level 5

--- strings/level5.js
+++ strings/level5.js
@@ -2,8 +2,8 @@
 const cardNumber = "123456789";
 Use .slice() to take the last 3 digits of the card number and combine them with stars to look like this: "******789".*/
 const cardNumber = "123456789";
-
-
+let output = cardNumber.toString().slice(6 ,9);
+console.log(output)
 
 
 
@@ -12,9 +12,11 @@ const bio = "I love coding. Coding is life.";
 Use the .split() method to find out how many words are in the bio string.
 Hint: Split by spaces and then check the .length.*/
 
+const bio = "I love coding. Coding is life."
+const words = bio.split(/\s+/) 
+const wordCount = words.length
 
-
-
+console.log(wordCount)
 
 
 
@@ -22,3 +24,8 @@ Hint: Split by spaces and then check the .length.*/
 const data = "Error: System failure at 5pm";
 Write a check to see if the string starts with the word "Error".
 Hint: Use the .startsWith() method.*/
+
+const data = "Error: System failure at 5pm"
+const startsWithError = data.startsWith("Error")
+
+console.log(startsWithError)
