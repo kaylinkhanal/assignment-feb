4eb4e3a5a9651d481a41f7980d1781e124483196
gomachand13 <you@example.com>
2026-02-18 16:29:22 +0545

    Level5.js

--- strings/level5.js
+++ strings/level5.js
@@ -4,23 +4,40 @@ const quote = 'Success usually comes to those who are too busy to be looking for
 // Q1. Output should be:
 // Luka Modric Is A Legendary Midfielder
 // 👉 Remove extra spaces and convert to Title Case.
-
+const formattedText = text2
+ .trim()
+  .replace(/\b\w/g, (l) => l.toUpperCase());
+console.log(formattedText);
 
 // Q2. Output should be:
 // success usually comes to those who are too busy to be looking for it.
 // 👉 Convert entire sentence to lowercase.
+console.log(quote.toLowerCase());
 
 
 // Q3. Output should be:
 // 2
 // 👉 Count how many times the word "to" appears.
+ const count = quote
+  .split(" ")
+  .filter(word => word === "to").length;
+console.log(count);  
 
 
 // Q4. Output should be:
 // relddifim yranegdel a si cirdom akul
 // 👉 Reverse the string.
+const reversed = text2
+  .split("")             // split into characters
+  .reverse()             // reverse the array of characters
+  .join("");             // join back into a string
+
+console.log(reversed);
 
 
 // Q5. Output should be:
 // true
 // 👉 Check if the quote ends with "it.".
+const result = quote.endsWith("it.");
+
+console.log(result);
