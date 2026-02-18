9710f409b7dbc1d0f3c6664e1605146b046ae3bd
Santosh Parajuli <santosh.08045@gmail.com>
2026-02-18 09:14:49 +0545

    solved array level 6

--- strings/level6.js
+++ strings/level6.js
@@ -2,10 +2,15 @@ const arr = ["apple", "banana", "grape", "orange", "kiwi"]
 
 
 // Q1 . expected output is "apple, banana, grape, orange, kiwi"
+const result1 = arr.join(", ");
+console.log(result1);
 
 // Q2. expected output is "APPLE"
-
+const result2 = arr[0].toUpperCase();
+console.log(result2);
 
 
 const scores  = "32,42,53"
 // Q3. Expected output is [32,42,53]
+const result3 = scores.split(",").map(Number);
+console.log(result3);
