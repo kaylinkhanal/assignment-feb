ff7688b20d1c003e6244ae7b5b442e2352ac6d18
gomachand13 <you@example.com>
2026-02-18 08:34:36 +0545

    Level4.js

--- strings/level4.js
+++ strings/level4.js
@@ -4,23 +4,22 @@ const email = 'haaland9@manchester.com';
 // Q1. Output should be:
 // Haaland
 // 👉 Extract the last name.
-
+const lastName = sentence.split(' ')[1];
+console.log(lastName);
 
 // Q2. Output should be:
 // haaland9
 // 👉 Extract username from email.
-
+const username = email.split('@')[0];
+console.log(username);
 
 // Q3. Output should be:
 // true
 // 👉 Check if sentence includes the word "powerful".
-
+console.log(sentence.includes("powerful")); 
 
 // Q4. Output should be:
-// Erling H.
-// 👉 Convert full name to short form.
-
-
-// Q5. Output should be:
 // Erling Haaland is a strong striker.
 // 👉 Replace "powerful" with "strong".
+let updatedsentence = sentence.replace("powerful", "strong");
+console.log(updatedsentence);
