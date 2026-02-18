5d629fe3ce4745025ecd067f4207ac57b65a1b0e
Alisa Bogati <alaskabogati@Alishas-MacBook-Air.local>
2026-02-19 02:57:01 +1100

    array level6,7 and string level 6

---
+++ strings/level6.js
@@ -0,0 +1,16 @@
+const arr = ["apple", "banana", "grape", "orange", "kiwi"]
+
+
+// Q1 . expected output is "apple, banana, grape, orange, kiwi"
+
+console.log(arr.join(', '))
+
+// Q2. expected output is "APPLE"
+console.log(arr[0].toUpperCase())
+
+
+const scores  = "32,42,53"
+// Q3. Expected output is [32,42,53]
+
+const scoresArray = scores.split(',').map(Number);
+console.log(scoresArray); // [32, 42, 53]
