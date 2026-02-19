48da4ae9891fb02357e56b3281b7485186bf95b8
Santosh Parajuli <santosh.08045@gmail.com>
2026-02-19 08:16:54 +0545

    Solved array level 7 questions

--- array/level7.js
+++ array/level7.js
@@ -2,16 +2,22 @@ const scores = ['ram',6,5,1]
 
 // Q1. use filter loop to remove strings from the scores array, expected output is [6,5,1]
 
-function loop(item){
-    if(item%2==0){
+function loop1(item){
+    if(typeof(item)=="number"){
             return item
     }
 }
 
-console.log(scores.filter(loop))
+console.log(scores.filter(loop1))
 
 const userScores = [['ram',60],['shyam',50],['hari',13]]
 
+function loop(item){
+    if(item[1]>20){
+            return item
+    }
+}
 
+console.log(userScores.filter(loop))
 // Q2. use filter loop to loop over each element and only keep the elements whose score is greater than 20,
 //  expected output is [['ram',60],['shyam',50]]
