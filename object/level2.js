dd5706ad7ddf7dd86e9a2a6039c0128997bb32bb
Aalok-sah14 <aaloksah1003@gmail.com>
2026-02-20 09:11:27 +0545

    answer of level 2

--- object/level2.js
+++ object/level2.js
@@ -17,8 +17,9 @@ const user = {
 }
 
 // Q1. output should be '123 Main St, Anytown, USA'
-
-
+const primaryAddressDetails = Object.values(user.address.primaryAddress);
+console.log(primaryAddressDetails);
 
 //Q2. output should be 2 (total number of values in address object )
 // hint: get the values of address object and find the length of it excluding null
+console.log(Object.values(user.address).filter(value => value !== null).length);
