7781a5db25b75fafdcb133c140781289450aa881
DikpalChaudhary <you@example.com>
2026-02-20 08:21:23 +0545

    level8

--- array/level8.js
+++ array/level8.js
@@ -22,3 +22,48 @@ const newNum = num.map((item)=>{
 })
 
 console.log(newNum)
+
+// Q3. Use filter to create a new array containing only numbers greater than 20,
+//  then use forEach to log each of those numbers to the console.
+
+const  greaterthan20 = num.filter((items)=>{
+        return items >20
+    
+})
+        greaterthan20.forEach(items=>console.log(items))
+
+//  Use map to create a new array where each number is replaced by the string
+//  "Even" if it's even, or "Odd" if it's odd.       
+
+const evenodd = num.map((number)=>{
+    if(number%2==0){
+        return "even"
+    }
+    else{
+        return "odd"
+    }
+})
+console.log(evenodd)
+
+
+// Q5. Use filter to find all numbers that are multiples of both 2 and 4, then use reduce to find their total sum.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const multipliesboth = numbers.filter((numb)=>{
+    return numb%2===0 && numb%4===0
+})
+console.log(multipliesboth)
+const totalsum =multipliesboth.reduce((prev, cuurent)=>{
+    return prev + cuurent
+})
+console.log(totalsum)
+
+// Q6. Use map to square every number in the array, then use filter to keep only the results that are less than 20.
+const squarenumber =numbers.map((square)=>{
+    return square**2
+})
+console.log(squarenumber)
+const result =squarenumber.filter((greatthan20)=>{
+    return greatthan20>20
+})
+console.log(result)
+
