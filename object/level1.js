7f375c72946b10beb982801d0588028ea9c7ed61
DikpalChaudhary <you@example.com>
2026-02-20 08:55:22 +0545

    object level1

--- object/level1.js
+++ object/level1.js
@@ -10,13 +10,28 @@ const scores = {
 // total of each sub is 50
 
 
+const totalsum =scores.math + scores.computer + scores.science
+console.log(totalsum)
+const percentage = (totalsum / 150) * 100;
+console.log(percentage)
+
+
+
 
 
 //Q2. convert object to array: left hand side
 //expected output is ['math', 'science', 'computer']
+const result= Object.keys(scores)
+console.log(result)
+
 
 //Q3. convert object to array: right hand side
 //expected output is [12, 42, 32]
+const marks=Object.values(scores)
+console.log(marks)
 
 //Q4. convert entire object to array
 //expected output is [['math', 12], ['science', 42], ['computer', 32]]
+
+const fulldata= Object.entries(scores)
+console.log(fulldata)
