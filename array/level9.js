bdee1e55de0fbfeb820c2df80e975b473ceaea91
kaylinkhanal10 <kyalinnop@gmail.com>
2026-02-20 09:23:37 +0545

    add array level9

---
+++ array/level9.js
@@ -0,0 +1,9 @@
+const userCarts = [
+    {orderid: 1, product: 'laptop', price: 50000, quantity: 1},
+    {orderid: 2, product: 'mobile', price: 20000, quantity: 2},
+    {orderid: 3, product: 'tablet', price: 15000, quantity: 1},
+    {orderid: 4, product: 'headphones', price: 3000, quantity: 3},
+]
+
+
+//Q1. use reduce loop to calculate the total amount spent by the user
