39eed2969729b8835632dbd62a396ae55197ef12
kaylinkhanal10 <kyalinnop@gmail.com>
2026-02-20 08:35:33 +0545

    added object prob

---
+++ object/level1.js
@@ -0,0 +1,22 @@
+const scores = {
+    math: 12,
+    science: 42,
+    computer: 32
+}
+
+
+
+//Q1. calculate the percentage of total marks obtained by the student, expected output is 62.67%
+// total of each sub is 50
+
+
+
+
+//Q2. convert object to array: left hand side
+//expected output is ['math', 'science', 'computer']
+
+//Q3. convert object to array: right hand side
+//expected output is [12, 42, 32]
+
+//Q4. convert entire object to array
+//expected output is [['math', 12], ['science', 42], ['computer', 32]]
