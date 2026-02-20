59bdaddb3b82fc048fd66daf3ae3877f6f986043
Aalok-sah14 <aaloksah1003@gmail.com>
2026-02-20 08:57:19 +0545

    answer of level 1

--- object/level1.js
+++ object/level1.js
@@ -9,14 +9,24 @@ const scores = {
 //Q1. calculate the percentage of total marks obtained by the student, expected output is 62.67%
 // total of each sub is 50
 
+const percentage = (scores.math + scores.science + scores.computer)/150*100
+console.log(percentage)
 
 
 
 //Q2. convert object to array: left hand side
 //expected output is ['math', 'science', 'computer']
+const subjects = Object.keys(scores)
+console.log(subjects)
+
 
 //Q3. convert object to array: right hand side
 //expected output is [12, 42, 32]
+const marks = Object.values(scores)
+console.log(marks)
+
 
 //Q4. convert entire object to array
 //expected output is [['math', 12], ['science', 42], ['computer', 32]]
+const array = Object.entries(scores)
+console.log(array)
