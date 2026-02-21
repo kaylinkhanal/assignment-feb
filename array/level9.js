88154cd5d3b62951e414c6ae9b90b2eae0d1a2a3
Alisa Bogati <alaskabogati@Alishas-MacBook-Air.local>
2026-02-21 18:05:35 +1100

    array level 9

---
+++ array/level9.js
@@ -0,0 +1,20 @@
+const userCarts = [
+    {orderid: 1, product: 'laptop', price: 50000, quantity: 1},
+    {orderid: 2, product: 'mobile', price: 20000, quantity: 2},
+    {orderid: 3, product: 'tablet', price: 15000, quantity: 1},
+    {orderid: 4, product: 'headphones', price: 3000, quantity: 3},
+]
+
+
+//Q1. use reduce loop to calculate the total amount spent by the user
+
+const temp = userCarts.reduce((acc, curr) => {
+   
+    
+    
+    acc= acc + (curr.price * curr.quantity)
+    return acc
+}, 0)
+
+
+console.log(temp)
